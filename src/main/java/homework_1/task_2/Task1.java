896883ecab2691e027e49664817681efa3e5433a
Diana <kartashevadidi@gmail.com>
2022-11-10 01:39:17 +0300

    test

---
+++ src/main/java/homework_1/task_2/Task1.java
@@ -0,0 +1,7 @@
+package homework_1.task_2;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello my friend, " + args[0] + " [Bday]:" + args[1]);
+    }
+}
