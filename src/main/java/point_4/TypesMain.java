91d03dffb0a553961c4ad98c122837e514ee1d60
Diana <kartashevadidi@gmail.com>
2022-11-09 23:54:59 +0300

    first commit

---
+++ src/main/java/point_4/TypesMain.java
@@ -0,0 +1,30 @@
+package point_4;
+
+public class TypesMain {
+    public static void main(String[] args) {
+ /*
+ byte  2^8 ->256 -> -128 до 127                  Ноль положительное число
+ short 2^16 65536  ->  -32768 до 32767
+ char  2^16-1 ( это символы)
+ int 2^32
+ long 2^64
+  */
+
+        /*
+        float
+        double
+         */
+
+        /*
+        boolean -> true/ false
+         */
+
+        /*
+        String
+         */
+
+        byte myByte;
+        myByte = 5;
+        System.out.println(myByte);
+    }
+}
