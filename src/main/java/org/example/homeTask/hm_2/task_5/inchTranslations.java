80186257f0c4faf0e0f45a49639d5992df79c6fd
Diana <kartashevadidi@gmail.com>
2022-11-14 05:03:18 +0300

    hm part 1.05

---
+++ src/main/java/org/example/homeTask/hm_2/task_5/inchTranslations.java
@@ -0,0 +1,14 @@
+package org.example.homeTask.hm_2.task_5;
+
+public class inchTranslations {
+    public static void main(String[] args) {
+        double centimetresInInch = 2.54;
+        System.out.println("Conversion from inches to centimeters");
+        for (double b = 0; b <= 20; b++) {
+            System.out.print(b + " = ");
+            System.out.println(b * centimetresInInch);
+        }
+    }
+
+
+}
