ea67c445420e7fa24eb6c1fed9669e56cee41b94
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-14 17:46:10 +0100

    Update Sport.java

--- src/main/java/org/example/homeTask/hm_2/task_1/Sport.java
+++ src/main/java/org/example/homeTask/hm_2/task_1/Sport.java
@@ -2,14 +2,14 @@ package org.example.homeTask.hm_2.task_1;
 
 public class Sport {
     public static void main(String[] args) {
-        double km = 10;
-        int day = 7;
-        double percent = 0.1;
+        double km = 10; // что такое км? defaultDailyDistance - думаю так информативнее будет
+        int day = 7; // это количество дней, а не конкретный день, поэтому days лучше
+        double percent = 0.1; // это не процент, а коэффициент дневного прироста. Процент - это 10%. Надо переименовать
         double sumTotal;
         sumTotal = km;
 
-        for (int i = 0; i < day; i++) {
-            km = (km + (km * percent));
+        for (int i = 0; i < day; i++) { // у тебя на 1 больше вышло
+            km = (km + (km * percent)); // (km * percent) - я думаю скобки не нужны
             sumTotal += km;
         }
         System.out.println(sumTotal);
