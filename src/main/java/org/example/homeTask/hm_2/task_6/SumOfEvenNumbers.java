0220a81d84e481923d1364836a110429b7d4f24f
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-15 08:40:34 +0100

    Update SumOfEvenNumbers.java

--- src/main/java/org/example/homeTask/hm_2/task_6/SumOfEvenNumbers.java
+++ src/main/java/org/example/homeTask/hm_2/task_6/SumOfEvenNumbers.java
@@ -2,9 +2,9 @@ package org.example.homeTask.hm_2.task_6;
 
 public class SumOfEvenNumbers {
     public static void main(String[] args) {
-
-        for (int i = 1; i <= 100; i++)
-            if (i % 2 != 1) {
+//эта строка лишняя
+        for (int i = 1; i <= 100; i++) // в конце этой строки должна быть "{". Всегда используй фигурные скобки для тела цикла
+            if (i % 2 != 1) { // твоя конструкция выглядит неоправданно сложно, проще ведь i % 2 == 0
                 System.out.println("HEllO figure " + i + "!");
             }
     }
