8b41b3a75ac8ea2575fbf8f96023f2997b0f78d0
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-15 08:05:47 +0100

    Update NoMultiplication.java

--- src/main/java/org/example/homeTask/hm_2/task_4/NoMultiplication.java
+++ src/main/java/org/example/homeTask/hm_2/task_4/NoMultiplication.java
@@ -4,6 +4,17 @@ package org.example.homeTask.hm_2.task_4;
 public class NoMultiplication {
     public static void main(String[] args) {
 
+    //суть ты уловила: умножение заменить сложением.
+    /* я запустил твою программу, судя по цифрам мы 10 должны умножить на 55? А вот что делает твоя программа я не понял.
+    Я хочу видеть две переменные и в конце результат их умножения, что-то вроде
+    
+    int firstNumber = 5;
+    int secodnNumber = 10;
+    
+    for....
+    
+    System.out.println(result);
+    */
         int a = 10;
         int c;
         for (c = 0; c < 55; c += a) {
