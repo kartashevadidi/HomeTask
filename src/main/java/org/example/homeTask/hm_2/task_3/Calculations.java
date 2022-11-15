c8dedcc73c6b1156ed7e7ed97cf2935444354aa4
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-15 07:56:13 +0100

    Update Calculations.java

--- src/main/java/org/example/homeTask/hm_2/task_3/Calculations.java
+++ src/main/java/org/example/homeTask/hm_2/task_3/Calculations.java
@@ -1,11 +1,12 @@
 package org.example.homeTask.hm_2.task_3;
 
 //отформатируй код
+// +
 public class Calculations {
     public static void main(String[] args) {
         int sum = 0;
         for (int i = 0; i <= 8; i++) {
-            sum = sum + (1 << i);
+            sum = sum + (1 << i); // лучше все-таки не использовать << или >> в коде
         }
         System.out.println(sum);
     }
