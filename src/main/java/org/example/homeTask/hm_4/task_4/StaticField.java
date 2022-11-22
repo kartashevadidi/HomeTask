061b37f16359bc3405ae170433537a985246a871
Alexander Fomin <RezviyBelorus@gmail.com>
2022-11-22 10:18:03 +0100

    Update StaticField.java

--- src/main/java/org/example/homeTask/hm_4/task_4/StaticField.java
+++ src/main/java/org/example/homeTask/hm_4/task_4/StaticField.java
@@ -4,13 +4,13 @@ import org.example.homeTask.hm_4.task_2.CustomerData;
 
 public class StaticField {
     public static void main(String[] args) {
-        CustomerData newClients = new CustomerData();
-        System.out.println(newClients.nameCustomer);
+        CustomerData newClients = new CustomerData(); // newClients во множественном числе, а по факту он там один. Такое название могло бы подойти для массива
+        System.out.println(newClients.nameCustomer); // я думаю эти данные можно было не печатать
         System.out.println(newClients.ageCustomer);
         newClients.setText("test_1");
         System.out.println(newClients.getText());
 
-        CustomerData oldClients = new CustomerData("Kate", 35);
+        CustomerData oldClients = new CustomerData("Kate", 35); // лучше не использовать в названиах префиксы new и old, если создать 3его кастомера, то какое название перееменной ему дать? newNewCustomer?
         System.out.println(oldClients.nameCustomer);
         System.out.println(oldClients.ageCustomer);
         oldClients.setText("test_2");
