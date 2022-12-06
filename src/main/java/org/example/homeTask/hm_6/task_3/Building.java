3fee1a0f3ccb6a469ce7e1d0ce54618a03dce625
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-06 18:32:06 +0100

    Update Building.java

--- src/main/java/org/example/homeTask/hm_6/task_3/Building.java
+++ src/main/java/org/example/homeTask/hm_6/task_3/Building.java
@@ -4,11 +4,11 @@ public interface Building {
 
     int getFloorsCount();
 
-    void turnHit(boolean on);
+    void turnHit(boolean on); // зачем аргумент, если он называется on? Тогда луше метод назвать turnHittingOn
 
     boolean isThereHeating();
 
     int getResidentsCount();
 
-
+//пустая строка не нужна
 }
