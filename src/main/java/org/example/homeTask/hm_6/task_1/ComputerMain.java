dd262b61ba316c09a52b0a71de1cf882417e2994
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-06 18:27:30 +0100

    Update ComputerMain.java

--- src/main/java/org/example/homeTask/hm_6/task_1/ComputerMain.java
+++ src/main/java/org/example/homeTask/hm_6/task_1/ComputerMain.java
@@ -1,5 +1,6 @@
 package org.example.homeTask.hm_6.task_1;
 
+// ComputerMain - мы так обычно называем классы с методом main() внутри. Вариантов названий много - Notebook,Laptop, MacBook, Tablet...
 public class ComputerMain implements Computer {
     private String isComputerOn;
     private String isComputerOff;
