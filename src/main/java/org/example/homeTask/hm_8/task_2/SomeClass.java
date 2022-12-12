8cc4d46f71d81e0d15413641d2723844906b608d
Alexander Fomin <RezviyBelorus@gmail.com>
2022-12-12 15:16:31 +0100

    Update SomeClass.java

--- src/main/java/org/example/homeTask/hm_8/task_2/SomeClass.java
+++ src/main/java/org/example/homeTask/hm_8/task_2/SomeClass.java
@@ -1,7 +1,7 @@
 package org.example.homeTask.hm_8.task_2;
 
-public class SomeClass {
-    private void someMethod() {
+public class SomeClass { // ну неееет, название надо придумать поинтереснее
+    private void someMethod() { // твой метод печатает текст, вот и назови его так. someMethod - не подходит
         System.out.println("Some text");
     }
 }
